4285fb8910bc8a394f7364c05325d44fca61a292
Maragatham Muthusamy <mahaa.shankar@gmail.com>
2022-05-05 21:47:53 -0400

    Added new version of code
    Added new version of code working set for a security module. Later we have to bring in all the code from the previous working folder to this project.

---
+++ src/main/java/com/maha/spring/EepsApp.java
@@ -0,0 +1,13 @@
+package com.maha.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EepsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EepsApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/maha/spring/EepsAppTests.java
@@ -0,0 +1,13 @@
+package com.maha.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
